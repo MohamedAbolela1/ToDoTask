471aa65f9195078e50688c0fb41bcd1119f9052d
Mohamed Abdulmohsen <Mohamed.Abdulmohsen@integrant.com>
2021-04-24 17:08:55 +0200

    feat:add task and retrieve data

--- app/src/main/java/com/ibtikar/todolisttask/ui/add_task/data/AddTaskRepositoryImpl.kt
+++ app/src/main/java/com/ibtikar/todolisttask/ui/add_task/data/AddTaskRepositoryImpl.kt
@@ -1,9 +1,14 @@
 package com.ibtikar.todolisttask.ui.add_task.data
 
 import com.ibtikar.todolisttask.data.local.dao.TasksDao
+import com.ibtikar.todolisttask.data.local.model.TaskEntity
 import com.ibtikar.todolisttask.ui.add_task.domain.repository.AddTaskRepository
 import javax.inject.Inject
 
 class AddTaskRepositoryImpl @Inject constructor(
     private val tasksDoa: TasksDao
-) : AddTaskRepository
+) : AddTaskRepository {
+    override suspend fun saveTask(taskTitle: String, taskDescription: String, date: Long) {
+        tasksDoa.addTask(TaskEntity(title = taskTitle, body = taskDescription, date = date))
+    }
+}
--- app/src/main/java/com/ibtikar/todolisttask/ui/add_task/domain/repository/AddTaskRepository.kt
+++ app/src/main/java/com/ibtikar/todolisttask/ui/add_task/domain/repository/AddTaskRepository.kt
@@ -1,3 +1,5 @@
 package com.ibtikar.todolisttask.ui.add_task.domain.repository
 
-interface AddTaskRepository
+interface AddTaskRepository {
+    suspend fun saveTask(taskTitle: String, taskDescription: String, time: Long)
+}
